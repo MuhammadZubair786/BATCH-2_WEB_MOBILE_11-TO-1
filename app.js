d55540f2afac4d2f32510ba607f522d964dc9ebb
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-06 16:29:18 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,79 +1,42 @@
-var array=[1,2,3,4,5,6,7]
-// console.log(array)
-// array.unshift(45)//add in start
-// console.log(array)
-// array.shift()//remove from start index
-// console.log(array)
+// var arr =[1,2,3,4,56,7,8]
+// console.log(arr.length)
+// // 0>7
+// for(var i=0;i<arr.length;i++){   
+//     console.log(arr[i])
+// }
 
-// delete array[3]
+// var arr =[]
 
-// console.log(array)
-// array[3]="smit"
-// console.log(array)
-// var array=[1,2,3,4,5,6,7]
-// var newdata= array.slice(2,6)
-// console.log(newdata)
-// array.splice(0,0,"asad","smit","hyd",34,56)
+// for(var i=0;i<5;i++){   
+//    var data = prompt("enter value")
+//    if(data==""){
+//     console.log("no enter data")
+//     break
+//    }
+//    else{
+//    arr.push(data)
+// }
+// }
 
-// console.log(array)
-//1,"asad","smit","hyd",34,56
-var arr =[1,2,45,56,78,45,35,45,67,89]
+// document.write(arr)
 
-arr.push("smit")
-console.log(arr)
+// for(var i=0;i<5;i++){
+//     if(i==1){
+//    break
+//     }
+//     console.log(i)
+// }
 
-arr.unshift("new course")
-console.log(arr)
+// var a ="Asad"
+// console.log(a.charCodeAt(0)
 
-arr.pop()
-console.log(arr)
+var inp =prompt("enter value")
 
-arr.shift()
-console.log(arr)
+for(i=0;i<inp.length;i++){
+    // console.log(inp[i])
+document.write("Ascii value of "+inp[i]+" is : "+ inp.charCodeAt(i)+"<br>")
 
-
-var index1= arr.indexOf(56)
-
-arr.splice( index1 ,1)
-console.log(arr)
-
-var index2= arr.indexOf(45,index1+1)
-
-arr.splice(index2 ,2,"data1 ","aple","laptop")
-console.log(arr)
-
-console.log(arr.length)
-
-// console.log(arr.sort())
-// console.log(arr)
-
-var data = arr.indexOf(45)
-console.log(data)
-
-arr.push(45)
-
-var newdata = arr.indexOf(45,data+1)
-console.log(newdata)
-
-
-
-console.log(arr)
-
-// var data = arr.lastIndexOf(45)
-// console.log(data)
-
-// var new1 = arr.includes("laptop")
-// console.log(new1)
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
