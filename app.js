01349a2f767e82919447a6b54782a171837823ad
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-06 16:28:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// var a =5
+// b = a++ + --a + a + a++ + ++a -a
+// 5 + 5 + 5 + 5 + 7 - 7
+//0
+/// 1 + 2 - 3
+//2
+//10 //7//0//
+//8
+//6
+//5
+//2
+//20
+// console.log(b)//1,3
+
+// var numb1 = parseFloat(prompt("enter numbe 1",10))
+// console.log(numb1)//2
+// var numb2 = parseFloat(prompt("enter number 2"))
+// console.log(numb2)//3
+
+// var sum = numb1+numb2 //5,23
+// console.log(sum)
