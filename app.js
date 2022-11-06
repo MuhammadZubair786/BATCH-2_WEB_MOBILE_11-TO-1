751270ad501a24beaae2b1ced5efaff1d1ac3eaa
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-06 16:27:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+// // var inp1 = parseInt(prompt("Enter First Number"))
+// // var inp2 = parseInt(prompt("Enter Second Number"))
+// // var op = prompt("Enter Opartor")
+
+// // if(op=="+"){
+// //     var sum = inp1+inp2
+// //     console.log(sum)
+// // }
+// // else if(op=="-"){
+// //     var sub = inp1-inp2
+// //     console.log(sub)
+// // }
+
+// var inp1=parseInt(prompt("Enter number"))
+// var inp2=parseInt(prompt("Enter number"))
+// var inp3=parseInt(prompt("Enter number"))
+// if(inp1>inp2 && inp1>inp3 ){
+//  document.write(inp1 +" is greater")
+//  if(inp2>inp3 ){
+//     document.write(inp2+"<br>is medium " )
+//     document.write(inp3 + " inp3 is small")
+//  }
+//  else if(inp2==inp3){
+//     document.write("2 AND 3 EQUAL")
+//  }
+//  else{
+//     document.write(inp3 +"  is medium ")
+//     document.write(inp2 +"  is small")
+
+//  }
+
+// }
+
+// else if(inp2>inp1 && inp2>inp3 ){
+//     document.write("<br>inp2 is greater")
+//     if(inp1>inp3 ){
+//        document.write("<br>inp1 is medium ")
+//        document.write(inp3 + " inp3 is small")
+//     }
+//     else if(inp1==inp3){
+//         document.write("1 AND 3 EQUAL")
+//      }
+//     else{
+//        document.write(inp3 +"  is medium ")
+//        document.write("<br>inp1 is small")
+   
+//     }
+   
+//    }
+//    else if(inp3>inp1 && inp3>inp2 ){
+//     document.write("<br>inp3 is greater")
+//     if(inp1>inp2 ){
+//        document.write(inp1+"<br> is medium ")
+//        document.write(inp2 +"  is small")
+//     }
+//     else if(inp1==inp2){
+//         document.write("1 AND 2 EQUAL")
+//      }
+//     else{
+//        document.write(inp2+"<br>is medium " )
+//        document.write("<br>inp1 is small")
+   
+//     }
+   
+//    }
+//    else if(inp1==inp2 && inp1==inp3){
+//     document.write("All equal")
+//    }
+//    else{
+//     console.log("enter correct darta ")
+//    }
+
+var a =[1,2,3,4,5,"data",true]
+// console.log(a.slice(3,6))
+// console.log(a.indexOf("dat"))
+console.log(a.indexOf("smit"))
+
+// console.log(a[5])
+// a.push(45,"asad",true)
+// console.log(a)
+// var remove = a.pop()
+// console.log(a)
+// console.log(remove)
+// console.log(a.length)
