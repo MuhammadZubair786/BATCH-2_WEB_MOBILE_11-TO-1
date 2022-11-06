031cb9b0ddc5363a3d0829e7635796fff1899565
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-06 16:28:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,81 @@
-// var a =5
-// b = a++ + --a + a + a++ + ++a -a
-// 5 + 5 + 5 + 5 + 7 - 7
-//0
-/// 1 + 2 - 3
-//2
-//10 //7//0//
-//8
-//6
-//5
-//2
-//20
-// console.log(b)//1,3
+var array=[1,2,3,4,5,6,7]
+// console.log(array)
+// array.unshift(45)//add in start
+// console.log(array)
+// array.shift()//remove from start index
+// console.log(array)
 
-// var numb1 = parseFloat(prompt("enter numbe 1",10))
-// console.log(numb1)//2
-// var numb2 = parseFloat(prompt("enter number 2"))
-// console.log(numb2)//3
+// delete array[3]
 
-// var sum = numb1+numb2 //5,23
-// console.log(sum)
+// console.log(array)
+// array[3]="smit"
+// console.log(array)
+// var array=[1,2,3,4,5,6,7]
+// var newdata= array.slice(2,6)
+// console.log(newdata)
+// array.splice(0,0,"asad","smit","hyd",34,56)
+
+// console.log(array)
+//1,"asad","smit","hyd",34,56
+var arr =[1,2,45,56,78,45,35,45,67,89]
+
+arr.push("smit")
+console.log(arr)
+
+arr.unshift("new course")
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+
+var index1= arr.indexOf(56)
+
+arr.splice( index1 ,1)
+console.log(arr)
+
+var index2= arr.indexOf(45,index1+1)
+
+arr.splice(index2 ,2,"data1 ","aple","laptop")
+console.log(arr)
+
+console.log(arr.length)
+
+// console.log(arr.sort())
+// console.log(arr)
+
+var data = arr.indexOf(45)
+console.log(data)
+
+arr.push(45)
+
+var newdata = arr.indexOf(45,data+1)
+console.log(newdata)
+
+
+
+console.log(arr)
+
+// var data = arr.lastIndexOf(45)
+// console.log(data)
+
+// var new1 = arr.includes("laptop")
+// console.log(new1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
