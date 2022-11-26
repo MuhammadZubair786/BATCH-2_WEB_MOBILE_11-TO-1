d0e7cb34911f038e5e393887abbb54011944d500
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2022-11-26 14:16:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
-// var arr =[1,2,3,4,56,7,8]
-// console.log(arr.length)
-// // 0>7
-// for(var i=0;i<arr.length;i++){   
-//     console.log(arr[i])
-// }
-
-// var arr =[]
-
-// for(var i=0;i<5;i++){   
-//    var data = prompt("enter value")
-//    if(data==""){
-//     console.log("no enter data")
-//     break
-//    }
-//    else{
-//    arr.push(data)
-// }
-// }
-
-// document.write(arr)
-
-// for(var i=0;i<5;i++){
-//     if(i==1){
-//    break
+// for(var i=0;i<10;i++){
+//     for(var j=0;j<10;j++){
+//      document.write("i :"+ i + " j :"+ j+"<br>")
 //     }
-//     console.log(i)
+    
+//     document.write(" <br>")
 // }
 
-// var a ="Asad"
-// console.log(a.charCodeAt(0)
+// var dept =[
+//     ["ALi","Owais","Ayesha","Danish","Ali","SMIT","ALI"],
+//     ["DAnIsH","danish","Ali","Bilal","Danish","Danish"],
+//     ["Saqib","BABER","alI"]
+// ]
 
-var inp =prompt("enter value")
+// // 00
+// for(var i=0;i<dept.length;i++){
+//     // document.write("array "+dept[i]+"<br>")
+//     for(j=0;j<dept[i].length;j++){
+//         // document.write(dept[i][j]+"<br>")
+//         if(dept[i][j].toUpperCase()=="ALI" || dept[i][j].toUpperCase()=="DANISH" ){
+            
+       
+//         if(dept[i][j].toUpperCase()=="ALI"){
+//             dept[i].splice(j,1,"Asad")
+//         }
+//         if(dept[i][j].toUpperCase()=="DANISH"){
+//             dept[i].splice(j,1,"OMER")
+//         }
+        
+//         j=j-1
+//         //   document.write(dept[i]+"<br>")
+     
+//         }
+//         // document.write(dept[i][j]+"<br>")
 
-for(i=0;i<inp.length;i++){
-    // console.log(inp[i])
-document.write("Ascii value of "+inp[i]+" is : "+ inp.charCodeAt(i)+"<br>")
+//     }
+// }
 
+// document.write(dept)
+
+
+var row = parseInt(prompt("Enter row"))
+var dept =[]
+ for(i=0;i<row;i++){
+    dept.push([])
+ }
+ console.log(dept)
+for(var i=0;i<row;i++){
+    var col = parseInt(prompt("Enter Col"))
+    for(var j=0;j<col;j++){
+        var inp = prompt("Enter data ")
+        dept[i].push(inp)
+    }
+    document.write(dept[i]+"<br>")
 }
-
-
-
-
 
