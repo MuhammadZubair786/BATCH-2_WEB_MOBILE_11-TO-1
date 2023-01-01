71426dc85367d5080fe9c66892519a8301c41233
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2023-01-01 13:36:46 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,56 +1,34 @@
-// for(var i=0;i<10;i++){
-//     for(var j=0;j<10;j++){
-//      document.write("i :"+ i + " j :"+ j+"<br>")
-//     }
-    
-//     document.write(" <br>")
-// }
+console.log(firebase.auth())
 
-// var dept =[
-//     ["ALi","Owais","Ayesha","Danish","Ali","SMIT","ALI"],
-//     ["DAnIsH","danish","Ali","Bilal","Danish","Danish"],
-//     ["Saqib","BABER","alI"]
-// ]
+var email = document.getElementById("email")
+var password = document.getElementById("password")
+var signup = document.getElementById("signup")
+var signin = document.getElementById("signin")
 
-// // 00
-// for(var i=0;i<dept.length;i++){
-//     // document.write("array "+dept[i]+"<br>")
-//     for(j=0;j<dept[i].length;j++){
-//         // document.write(dept[i][j]+"<br>")
-//         if(dept[i][j].toUpperCase()=="ALI" || dept[i][j].toUpperCase()=="DANISH" ){
-            
-       
-//         if(dept[i][j].toUpperCase()=="ALI"){
-//             dept[i].splice(j,1,"Asad")
-//         }
-//         if(dept[i][j].toUpperCase()=="DANISH"){
-//             dept[i].splice(j,1,"OMER")
-//         }
-        
-//         j=j-1
-//         //   document.write(dept[i]+"<br>")
-     
-//         }
-//         // document.write(dept[i][j]+"<br>")
-
-//     }
-// }
-
-// document.write(dept)
+signup.addEventListener("click",function(){
+    console.log(email.value)
+    console.log(password.value)
+    firebase.auth().createUserWithEmailAndPassword(email.value,password.value)
+    .then((userdata)=>{
+        console.log(userdata.user.uid)
+    })
+    .catch((err)=>{
+        // console.log(err)
+        alert(err)
+    })
+})
 
 
-var row = parseInt(prompt("Enter row"))
-var dept =[]
- for(i=0;i<row;i++){
-    dept.push([])
- }
- console.log(dept)
-for(var i=0;i<row;i++){
-    var col = parseInt(prompt("Enter Col"))
-    for(var j=0;j<col;j++){
-        var inp = prompt("Enter data ")
-        dept[i].push(inp)
-    }
-    document.write(dept[i]+"<br>")
-}
+signin.addEventListener("click",function(){
+    console.log(email.value)
+    console.log(password.value)
+    firebase.auth().signInWithEmailAndPassword(email.value,password.value)
+    .then((userdata)=>{
+        console.log(userdata.user.uid)
+    })
+    .catch((err)=>{
+        // console.log(err)
+        alert(err)
+    })
+})
 
